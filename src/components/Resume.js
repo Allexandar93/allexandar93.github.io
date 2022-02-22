d55f6955c6094fff3a09c10dad4511c6bddbcdec
Aleksandar Zivotic <zivoticaleksandar00@gmail.com>
2022-02-22 22:38:48 +0100

    update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,10 +6,10 @@ function Resume() {
   return (
     <div id="resume" className="resume">
       <p className="resumeText">
-        Click on the image to view and download my resume
+        Click on the image below to download my resume
       </p>
       <a
-        href="https://drive.google.com/file/d/1n-qKBImBsJPHxo9F0SFRBJiijDllFsHp/view?usp=sharing"
+        href="https://drive.google.com/uc?export=download&id=1D_f3D4z3Tk6yltMR1UH6xX2nn5e2-GIg"
         target="_blank"
       >
         <img className="resumeImg" src={CV} />
