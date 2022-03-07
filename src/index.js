7db66edb8f892762cf4d4f1c4febb48f4488cd0b
Aleksandar Zivotic <zivoticaleksandar00@gmail.com>
2022-03-07 14:52:29 +0100

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/allexandar93.github.io">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
