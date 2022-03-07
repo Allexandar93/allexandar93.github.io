a1c4f6584655f62557c42db961e8dbd719cfd6d6
Aleksandar Zivotic <zivoticaleksandar00@gmail.com>
2022-03-07 14:42:04 +0100

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
-    <Routes basename="/allexandar93.github.io ">
+    <Routes basename="/porfolio">
       <Route path="/" element={<Home />} />
       <Route path="/portfolio" element={<Portfolio />} />
     </Routes>
