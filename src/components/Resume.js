60301e7156a1fdedf8d9be13a45945a0758be2ca
Aleksandar Zivotic <zivoticaleksandar00@gmail.com>
2022-03-08 16:44:59 +0100

    update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -9,7 +9,7 @@ function Resume() {
         Click on the image below to download my resume
       </p>
       <a
-        href="https://drive.google.com/uc?export=download&id=1D_f3D4z3Tk6yltMR1UH6xX2nn5e2-GIg"
+        href="https://drive.google.com/uc?export=download&id=14MqXitXfhQZWG8vHkjKyLeTVy51OAkdw"
         target="_blank"
       >
         <img className="resumeImg" src={CV} />
